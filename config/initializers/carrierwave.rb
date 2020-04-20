583d5617fba5f22a11d593857ae93b8483d3fb45
Tcaprioli <Capriolityler@gmail.com>
2020-04-20 14:01:26 -0400

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,6 @@ CarrierWave.configure do |config|
       provider:                         'Google',
       google_storage_access_key_id:     'xxxxxx',
       google_storage_secret_access_key: 'yyyyyy'
-    }
+    } 
     config.fog_directory = 'name_of_directory'
   end
