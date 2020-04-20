34c552192835e732eb733298ab32fb700c9b2102
Tcaprioli <Capriolityler@gmail.com>
2020-04-20 14:06:08 -0400

    test

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider:                         'Google',
-      google_storage_access_key_id:     'xxxxxx',
-      google_storage_secret_access_key: 'yyyyyy'
-    } 
-    config.fog_directory = 'name_of_directory'
-  end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
